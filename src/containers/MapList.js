908839fa0a3d426158fde971823fce3a82e17656
Bruno Garcia <bruno@garciaechegaray.com>
2018-05-17 11:02:39 +0200

    Get cars and taxis data from MapList component

--- src/containers/MapList.js
+++ src/containers/MapList.js
@@ -1,8 +1,64 @@
 import React, { Component } from 'react';
+import api from '../api';
+import Error from '../components/Error';
+import Loading from '../components/Loading';
 
 class MapList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: false,
+      vehicles: {
+        cars: [],
+        taxis: [],
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.isAlreadyMounted = true;
+    this.getVehicles();
+  }
+
+  componentWillUnmount() {
+    this.isAlreadyMounted = false;
+  }
+
+  getVehicles() {
+    Promise.all([api.getCars(), api.getTaxis()])
+      .then((data) => {
+        const [cars, taxis] = data;
+        if (this.isAlreadyMounted) {
+          this.setState({
+            loading: false,
+            vehicles: {
+              cars,
+              taxis,
+            },
+          });
+        }
+      }).catch(() => {
+        if (this.isAlreadyMounted) {
+          this.setState({
+            error: true,
+          });
+        }
+      });
+  }
+
   render() {
-    return 'Map list';
+    const { error, loading, vehicles } = this.state;
+
+    if (error) {
+      return <Error />;
+    } else if (loading) {
+      return <Loading />;
+    }
+
+    console.log(vehicles);
+
+    return 'map list';
   }
 }
 
