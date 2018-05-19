0237536db5269d346b8764d969b5c65e7eb4bae7
Bruno Garcia <bruno@garciaechegaray.com>
2018-05-19 18:32:56 +0200

    Update CarList and TaxiList unit tests

--- src/components/TaxiList.test.js
+++ src/components/TaxiList.test.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import TaxiList from './TaxiList';
+import TaxiItem from './TaxiItem';
+
+const props = {
+  data: [
+    {
+      id: 12345,
+      coordinate: {
+        latitude: 41,
+        longitude: 52,
+      },
+      state: 'test',
+      type: 'test',
+    },
+    {
+      id: 56789,
+      coordinate: {
+        latitude: 33,
+        longitude: 82,
+      },
+      state: 'test',
+      type: 'test',
+    },
+  ],
+};
 
 test('renders correctly', () => {
-  const props = {
-    data: [
-      {
-        id: 12345,
-        coordinate: {
-          latitude: 41,
-          longitude: 52,
-        },
-        state: 'test',
-        type: 'test',
-      },
-      {
-        id: 56789,
-        coordinate: {
-          latitude: 33,
-          longitude: 82,
-        },
-        state: 'test',
-        type: 'test',
-      },
-    ],
-  };
-
   const component = renderer.create(<TaxiList {...props} />);
-
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('renders a couple of TaxiItem components', () => {
+  const wrapper = shallow(<TaxiList {...props} />);
+  expect(wrapper.find(TaxiItem).length).toBeGreaterThanOrEqual(props.data.length);
 });
